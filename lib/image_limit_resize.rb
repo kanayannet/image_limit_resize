8376654857fc77743798b330528585c3efcabc54
Hiroaki Kanazawa <kanayannet@gmail.com>
2011-09-28 02:16:23 +0900

    画像 script のアップ

---
+++ lib/image_limit_resize.rb
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+
+#image magick limit resize author @kanayannet
+
+class ImageLimitResize
+
+	require "RMagick"
+
+	def initialize(file = "")
+		@img = Magick::Image.read(file).first
+	end
+	
+	def set_limit(size = 0)
+		@size = size
+		return self
+	end
+
+	def resize(file = "")
+		width = @img.columns
+		height = @img.rows
+		ret = limit_convert({:width => width,
+				     :height => height})
+		image = @img.resize(ret[:width],ret[:height])
+		image.write(file)
+		return true
+	end
+	
+	private
+	
+	def limit_convert(refs = {})
+		width = refs[:width]
+		height = refs[:height]
+		if @size < width
+			height = ((@size.to_f / width.to_f) * height).to_i
+			width = @size
+		end
+		if @size < height
+			width = ((@size.to_f / height.to_f) * width).to_i
+			height = @size
+		end
+		return {:width => width,:height => height}
+	end
+end
---
+++ test/image_limit_resize_spec.rb
@@ -0,0 +1,18 @@
+#!/usr/local/bin/ruby
+# -*- coding: UTF-8 -*-
+
+require 'rspec'
+require './lib/image_limit_resize.rb'
+
+describe "ImageLimitResize test" do
+	before do
+		filename = 'sakura.jpg'
+		@obj = ImageLimitResize.new(filename)
+	end
+	it "limit 40px で resize できるか?" do
+		@obj.set_limit(90).resize("sakura_resize.jpg").should == true
+		img = Magick::Image.read("sakura_resize.jpg").first
+		img.columns.should <= 90
+		img.rows.should <= 90
+	end
+end
