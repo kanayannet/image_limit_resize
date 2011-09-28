567f5aa6f7bdf22e5759a75064bfecccdc297ba6
Hiroaki Kanazawa <kanayannet@gmail.com>
2011-09-28 12:13:57 +0900

    テストコードにコメント追加

--- test/image_limit_resize_spec.rb
+++ test/image_limit_resize_spec.rb
@@ -11,6 +11,8 @@ describe "ImageLimitResize test" do
 	end
 	it "limit 90px で resize できるか?" do
 		@obj.set_limit(90).resize("sakura_resize.jpg").should == true
+
+		#Rmagick を直接読んでサイズチェック
 		img = Magick::Image.read("sakura_resize.jpg").first
 		img.columns.should <= 90
 		img.rows.should <= 90
